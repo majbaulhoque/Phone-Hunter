78bcef489a2e7365611b61613d2cd4b67c2e64be
majba-ul-hoque <majba9399@gmail.com>
2023-09-04 17:38:17 +0600

    API image convet dynamically in UI

--- phone.js
+++ phone.js
@@ -1,7 +1,34 @@
 const loadPhone = async () =>{
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
-    console.log(data);
+    const phone = data.data;
+    displayPhones(phone);
+}
+
+const displayPhones = input =>{
+    console.log(input);
+
+    const phoneContainer = document.getElementById('phone-container');
+
+    input.forEach(phones =>{
+        // console.log(phones);
+        // 2. Create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+        // set inner HTML
+        phoneCard.innerHTML = `
+        <figure><img src="${phones.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+        <h2 class="card-title">${phones.phone_name}</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+        </div>
+        </div>
+        `;
+        // 4 Append Child
+        phoneContainer.appendChild(phoneCard);
+    })
 }
 
 loadPhone();
